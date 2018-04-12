30e295ee86968c78139b17c8ed7030fc4c7247df
Kamideux <matthieu.orriere@gmail.com>
2018-04-12 20:39:45 +0200

    CLIENT + SERVEUR

--- code/server/serveur.py
+++ code/server/serveur.py
@@ -1,18 +1,37 @@
 from socket import *
 import pickle
-import sys
+import sys,os
 
-def storeClient(nomUser):
-    listeUser = {
-        nomUser : 0
-    }
-    with open('donnees.txt', 'wb') as fichier:
-        pick = pickle.Pickler(fichier)
-        pick.dump(nomUser)
-    with open('donnees.txt', 'rb') as fichier:
-        mon_depickler = pickle.Unpickler(fichier)
-        score_recupere = mon_depickler.load()
-        print (score_recupere)
+def storeClient(adr,nomUser):
+    ## TODO: Verif si c'est le bon user avec l'IP et le nom
+    with open('./donnees.json', 'rb') as fichier:
+        data = pickle.load(fichier)
+        if type(data) == dict:
+            data[adr] = [nomUser,"play","null"]
+    with open('./donnees.json','wb') as fichier:
+        pickle.dump(data,fichier)
+    with open('./donnees.json', 'rb') as fichier:
+        data = pickle.load(fichier)
+        print(data)
+
+def initFile():
+    try:
+        with open('./donnees.json'):
+            pass
+    except IOError:
+        users = {}
+        with open('./donnees.json', 'wb') as fichier:
+            pickle.dump(users,fichier)
+
+def initRobot(adr):
+    with open('./donnees.json', 'rb') as fichier:
+        data = pickle.load(fichier)
+        data[adr][2] = {"x" : 1, "y" : 0, "res" : 0}
+    with open('./donnees.json','wb') as fichier:
+        pickle.dump(data,fichier)
+    with open('./donnees.json', 'rb') as fichier:
+        data = pickle.load(fichier)
+        print(data)
 
 
 #creation socket
@@ -20,16 +39,24 @@ sock_server = socket()
 sock_server.bind(('',  int(sys.argv[1]) ))
 sock_server.listen(5)
 
+initFile()
 sock_client, adr_client = sock_server.accept()
 
 while True:
     cmd = sock_client.recv(255)
-    if cmd.decode().upper() == "CONNECT":
-        sock_client.send(b"Connexion ....")
+    tab = cmd.decode().split(" ")
+    print (tab)
+    if tab[0].upper() == "INIT":
+        if len(tab) != 3:
+            sock_client.send(b"Nombre parametre incorrect .... NOK")
+        else:
+            initRobot(adr_client[0],tab[1],tab[2])
+            sock_client.send(b"Initialisation .... OK")
     else :
-        storeClient(cmd.decode().upper())
-        sock_client.send(b"Commande inexistante")
-    print(cmd.decode())
+        storeClient(adr_client[0],tab[0])
+        sock_client.send(b"Stockage du client .... OK")
+
+    #print(cmd.decode())
 
 sock_server.shutdown(SHUT_RDWR)
 for t in threading.enumerate():
