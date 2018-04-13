066604e74e8308a3c6a19c1995cc5b87ec28e729
Denis <guiraudet@live.fr>
2018-04-13 09:42:31 +0200

    clean kasher

--- code/client/src/App.vue
+++ code/client/src/App.vue
@@ -1,26 +1,23 @@
 <template>
   <div id="app">
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'App'
+}
 </script>
 
 <style>
-* {
-  padding: 0;
-  margin: 0;
-}
 #app {
-  position: relative;
-  min-width: 100vw;
-  width: 100vw;
-  max-width: 100vw;
-  min-height: 100vh;
-  height: 100vh;
-  max-height: 100vh;
-  overflow: hidden;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
